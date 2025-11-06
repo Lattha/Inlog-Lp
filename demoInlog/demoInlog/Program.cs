e40cdbdd28a32bdad24d798757f593b3c082df16
LatthakonePathavongPXL <latthakone.pathavong@student.pxl.be>
2025-11-06 13:08:59 +0100

    DEMO-01

---
+++ demoInlog/demoInlog/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace demoInlog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Gebruikersnaam: ");
+            string userName = Console.ReadLine();  
+        }
+    }
+}
